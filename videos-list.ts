78cbe5a1e18712e5f5f1d6688ba9bb2f358c6beb
Paul Lyons <profpylons@users.noreply.github.com>
2023-09-21 23:41:25 +0300

    make the names line up with the index

--- videos-list.ts
+++ videos-list.ts
@@ -12,32 +12,32 @@ export function getVideos(): Video[] {
 const videos: Video[] = [
   {
     index: 0,
-    name: 'First Video',
+    name: 'Video Zero',
     url: 'https://somewhere/0'
   },
   {
     index: 1,
-    name: 'Second Video',
+    name: 'Video One',
     url: 'https://somewhere/1'
   },
   {
     index: 2,
-    name: 'Third Video',
+    name: 'Video Three',
     url: 'https://somewhere/2'
   },
   {
     index: 3,
-    name: 'Fourth Video',
+    name: 'Video Four',
     url: 'https://somewhere/3'
   },
   {
     index: 4,
-    name: 'Fifth Video',
+    name: 'Video Five',
     url: 'https://somewhere/4'
   },
   {
     index: 5,
-    name: 'Sixth Video',
+    name: 'Video Six',
     url: 'https://somewhere/5'
   },
 ];
